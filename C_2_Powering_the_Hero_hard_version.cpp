816dcd83f1a2f48ae58391977254df56389b99a8
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2026-05-24 11:32:31 +0600

    Add files via upload

---
+++ C_2_Powering_the_Hero_hard_version.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        priority_queue<long long> pq;
+        long long ans = 0;
+        for(int i=0; i<n; i++) 
+        {
+            long long s;
+            cin >> s;
+            if (s > 0) 
+            {
+                pq.push(s);
+            } 
+            else 
+            {
+                if(!pq.empty()) 
+                {
+                    ans += pq.top();
+                    pq.pop();
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
